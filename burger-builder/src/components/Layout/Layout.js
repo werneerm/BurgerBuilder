58e40bcd2eae6b6b4080ad97cbaec2bb010d2281
werneerm <wmarcos25@gmail.com>
2021-02-27 16:25:54 +0100

    sidedrawer for mobiles halfway done

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from "./Layout.css";
 
 const Layout = (props) => (
 	<Aux>
 		<Toolbar />
+		<SideDrawer />
 		{/* <div>Toolbar,SideDrawer,Backdrop</div> */}
 		<main className={classes.Content}>{props.children}</main>
 	</Aux>
--- burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 import classes from "./Toolbar.css";
 const Toolbar = (props) => (
 	<header className={classes.Toolbar}>
 		<div>MENU</div>
-		<Logo />
-		<nav>...</nav>
+		<div className={classes.Logo}>
+			<Logo />
+		</div>
+		<nav className={classes.DesktopOnly}>
+			<NavigationItems />
+		</nav>
 	</header>
 );
 
