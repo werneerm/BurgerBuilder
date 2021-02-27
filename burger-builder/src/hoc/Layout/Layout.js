d4ec2155d42e8b075c40d258dfb710ad7f2a5a6f
werneerm <wmarcos25@gmail.com>
2021-02-27 17:20:52 +0100

    changed the folder structure

--- burger-builder/src/hoc/Layout/Layout.js
+++ burger-builder/src/hoc/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import Aux from "../../hoc/Aux";
-import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
-import Toolbar from "../Navigation/Toolbar/Toolbar";
+import Aux from "../Aux";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 import classes from "./Layout.css";
 
 class Layout extends Component {
