32aec580e46ad229d4899c303752cf9748744f1d
werneerm <wmarcos25@gmail.com>
2021-03-02 13:58:48 +0100

    app is connected with firebase and save order on the db

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
 	salad: 0.5,
@@ -89,7 +90,28 @@ export default class BurgerBuilder extends Component {
 	};
 
 	purchaseContinueHandler = () => {
-		alert("You continue!");
+		// alert("You continue!");
+		// Price would be stored on server only in this case its here
+		const order = {
+			ingredients: this.state.ingredients,
+			price: this.state.totalPrice,
+			customer: {
+				name: "Werner Márk",
+				address: {
+					street: "Teststreet 1",
+					zipCode: "1234",
+					country: "Saint Kittis and Nevis",
+				},
+				email: "test@test.com",
+			},
+			deliveryMethod: "fastest",
+		};
+		axios
+			.post("/orders.json", order)
+			.then((response) => console.log(response))
+			.catch((error) => {
+				console.log(error);
+			});
 	};
 	render() {
 		const disabledInfo = {
